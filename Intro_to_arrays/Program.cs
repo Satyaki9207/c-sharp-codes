fbf35b46705313e112ac114bc251f3f07ce01b66
Satyaki Ray <xlr892@gmail.com>
2023-05-06 13:10:25 +0900

    added arrays and functions

--- Intro_to_arrays/Program.cs
+++ Intro_to_arrays/Program.cs
@@ -4,7 +4,19 @@ using System.Runtime.Serialization;
 namespace Intro2Arrays
 {
     public class program
-    {
+
+    {   //-----------------FUNCTIONS---------------------
+        static void printArray(int[] arr,string msg)
+            //Function to cycle through the array and print elements
+        {
+            foreach(int k in arr)
+            {
+                Console.WriteLine("{0}:{1}", msg, k);
+            }
+        }
+        //-------------END OF FUNCTIONS------------------
+
+
         static void Main()
         {
             int[] favNums = new int[3]; //arrays in C# have fixed sizes
@@ -30,6 +42,28 @@ namespace Intro2Arrays
                 {"Smith","Hector"} };
             // accessing elements inside multi dimensional array
             Console.WriteLine("Array element at pos (0,0) is {0}", custnames.GetValue(0, 0));
+
+            //creating 3-d array
+            Console.WriteLine();
+            int[] randNums = { 1, 4, 2, 9, 6 };
+            printArray(randNums, "forEach");
+
+            //Sorting Arrays
+            Array.Sort(randNums);
+            Array.Reverse(randNums);
+            Console.WriteLine("1 at index: {0}",Array.IndexOf(randNums,1));
+
+            //changing value of elements in an array
+            randNums.SetValue(0, 1);
+            int[] srcArray = { 1, 2, 3 };
+            int[] destArray = new int[2];
+            Console.WriteLine();
+            int start_idx=0, length=2;
+            Array.Copy(srcArray, start_idx,destArray,0,length);
+            printArray(destArray, "Copy");
+
+            
+
 
         }
     }
